0d8afebb0cc11d214d4d347a536c31b600bb8af5
abdul <abdulsk054@gmail.com>
2023-11-22 11:49:05 +0530

    replacing strings with passwords

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,18 +1,18 @@
 greeting() {
-  echo -e "\e[31m Hello, Good Morning \e[0m"
-  echo -e "\e[32m Welcome to the DevOps Training \e[0m"
+  echo -e "\e[33m Hello, Good Morning \e[0m"
+  echo -e "\e[33m Welcome to the DevOps Training \e[0m"
   return 10
   echo -e "\e[33m This is our starting day of Training Session \e[0m"
 }
 
 greeting
 
-echo -e "\e[34m function exit status - $?\e[0m"
+echo -e "\e[33m function exit status - $?\e[0m"
 
 input() {
-  echo first arguments - $1
-  echo second arguments - $2
-  echo no of arguments - $*
-  echo all arguments - $#
+  echo -e "\e[33m first arguments - $1 \e[0m"
+  echo -e "\e[33m second arguments - $2 \e[0m"
+  echo -e "\e[33m no of arguments - $* \e[0m"
+  echo -e "\e[33m all arguments - $# \e[0m"
 }
 input abc 1234 abdul

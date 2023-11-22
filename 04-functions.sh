09a4bfc625fafa210b88aa610c32e6c31286a6ce
abdul <abdulsk054@gmail.com>
2023-11-22 11:42:55 +0530

    replacing strings with passwords

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,9 +1,9 @@
 greeting() {
-  echo Hello, Good Morning
-  echo Welcome to the DevOps Training
-  echo This is our starting day of Training Session
+  echo -e "\e[31m Hello, Good Morning \e[0m"
+  echo -e "\e[32m Welcome to the DevOps Training \e[0m"
+  echo -e "\e[33m This is our starting day of Training Session \e[0m"
 }
 
 greeting
 
-echo -e "\e[32m function exit status - $?\e[0m"
+echo -e "\e[34m function exit status - $?\e[0m"

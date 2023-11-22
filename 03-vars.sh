4be8bb14ef3b3b2588f839f2f4f337669e6c9444
abdul <abdulsk054@gmail.com>
2023-11-22 11:33:38 +0530

    replacing strings with passwords

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,2 +1,17 @@
+# variables
 a=10
 echo a is $a
+
+# Special Variables
+# $0 - $N, $#, $*
+
+# Substitution Variables
+# we wanted to store a output for further useage
+
+#  Command Substitution
+DATE=$(date)
+echo today date is $DATE
+
+# Arithematic Substitution
+ADD=$(( 2+2 ))
+echo ADD of 2+2 = $ADD
